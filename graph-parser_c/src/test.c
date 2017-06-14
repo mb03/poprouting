898cd81c18c2e7cc30417ac7638abb8364f53ae3
mb03 <mb0392@gmail.com>
2017-06-14 22:30:35 +0200

    minor changes

--- graph-parser_c/src/test.c
+++ graph-parser_c/src/test.c
@@ -56,8 +56,89 @@ char* read_file_content(char *filename)
     return buffer;
 }
 
+struct graph * read_graph_from_file(char * source_file){
+    FILE * fp;
+    char * line = NULL;
+    size_t len = 0;
+    ssize_t read;
+    
+    fp = fopen(source_file, "r");
+    if (fp == NULL)
+        exit(EXIT_FAILURE);
+    struct graph * g=(struct graph *)malloc(sizeof(struct graph));
+    init_graph(g);
+    while ((read = getline(&line, &len, fp)) != -1) {
+        char source[256], target[256];
+        float cost;
+        int n=sscanf( line, "%s %s %f", source, target, &cost);
+        if(n==2){
+            add_edge_graph(g,source,target, 1,false);
+        }else if(n==3){
+            add_edge_graph(g,source,target, cost,false);
+        }
+    }
+    
+    fclose(fp);
+    if (line)
+        free(line);
+    return g;
+}
+#include <sys/time.h>
+long getMicrotime(){
+	struct timeval currentTime;
+	gettimeofday(&currentTime, NULL);
+	return currentTime.tv_sec * (int)1e6 + currentTime.tv_usec;
+}
+double run_test( bool heuristic, bool thread,struct graph * g) {
+    multithread=thread;
+   
+    double * bc;
+    int s = time(NULL);
+clock_t t = clock();
+struct timeval tv;
+gettimeofday(&tv,NULL);
+    unsigned long int time = getMicrotime();
+    if(heuristic){
+        bc=(double*)betwenness_heuristic(g,recursive);
+    }else{
+        bc=betweeness_brandes(g,true,0);
+    }
+    t = clock() - t;
+    time =getMicrotime()-time;
+    printf("realtime  %lu\n",time);
+    return ((double)t)/CLOCKS_PER_SEC;
+}
 
-
+int main(int argc, char** argv) {
+if(argc<3){
+        printf("Usage a.out heu thredead. E.g. ./a.out 1 1. Exiting.\n");
+        return 0;
+    }
+bool h,t;
+h=atoi(argv[1])==1;
+t=atoi(argv[2])==1;
+struct graph * g=read_graph_from_file("input.edgelist");
+double res=0;
+int repetitions=10;
+double *values=(double*)malloc(sizeof(double)*repetitions);
+int i=0;
+for(;i<repetitions;i++){
+double r=run_test(h,t,g);
+printf("exe %d %f \n",i,r);
+values[i]=r;
+res+=r;
+}
+double m=res/repetitions;
+double var=0;
+for(i=0;i<repetitions;i++){
+double std=m-values[i];
+var+=std*std;
+}
+var/=repetitions;
+printf("res %f +-(%f)\n",m,var);
+free(values);
+}
+/*
 int main(int argc, char** argv) {
     if(argc==1){
         printf("I need to know if i have to use heuristic. Exiting.\n");
@@ -77,7 +158,8 @@ int main(int argc, char** argv) {
     graph_parser_parse_simplegraph(cgp,topo);
     graph_parser_calculate_bc(cgp);
     map_id_degree_bc * bc_degree_map = (map_id_degree_bc *) malloc(sizeof(map_id_degree_bc));
-    graph_parse_compose_degree_bc_map(cgp,bc_degree_map);
+    
+    graph_parser_compose_degree_bc_map(cgp,bc_degree_map);
     
     int i;
     // printf("[\n");
@@ -94,4 +176,4 @@ int main(int argc, char** argv) {
     printf("}\n");
     delete_graph_parser(cgp);
     return (EXIT_SUCCESS);
-}
+}*/
